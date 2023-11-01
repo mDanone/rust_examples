e862541f21477c4f708bfd5f4aa1bee7cf2e5220
ext-daniyalov-dg <ext-daniyalov-dg@zvuk.com>
2023-11-01 07:35:37 +0300

    control flow examples

--- src/main.rs
+++ src/main.rs
@@ -38,8 +38,55 @@ fn test_enums(){
     
     let some_number = Some(5);
     let some_char = Some('h');
-
     let absent_number: Option<i32> = None;
+}
+
+
+#[derive(Debug)]
+enum UsState{
+    Alabama,
+    Alaska
+}
+
+enum Coin {
+    Penny,
+    Nickel,
+    Dime,
+    Quarter(UsState)
+}
+
+fn value_in_cents(coin: Coin) -> u8 {
+    match coin {
+        Coin::Penny => {
+            println!("Lucky Penny");
+            1
+        }
+        Coin::Nickel => 5,
+        Coin::Dime => 10,
+        Coin::Quarter(state)=> {
+            println!("State quarter from {:?}!", state);
+            25
+        }
+    }
+}
+
+
+fn plus_one(x: Option<i32>) -> Option<i32> {
+    match x {
+        None => None,
+        Some(i) => Some(i + 1),
+    }
+}
+
+
+fn test_control_flow(){
+    value_in_cents(Coin::Quarter(UsState::Alabama));
+    
+    let five = Some(5);
+    let six = plus_one(five);
+    let none = plus_one(None);
+
+    println!("{}", six.unwrap());
 }
 
 
@@ -99,4 +146,5 @@ fn main() {
     test_vectors();
     test_strings_in_utf();
     test_enums();
+    test_control_flow();
 }
